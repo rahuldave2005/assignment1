11de5881a5b6c0c796ace6bf1329a0259d9eeb97
rahuldave2005 <daverahul6181@gmail.com>
2026-01-15 18:40:35 +0530

    modularization of code

--- src/com/tss/assignment1/SmartCityUtilitySystem.java
+++ src/com/tss/assignment1/SmartCityUtilitySystem.java
@@ -56,71 +56,105 @@ public class SmartCityUtilitySystem {
         System.out.println("4. Back");
 
         int internetChoosenNumber=scanner.nextInt();
-        if(internetChoosenNumber==4)return;
-        System.out.println("Enter Duration in months (1/3/6): ");
-        int monthDuration=scanner.nextInt();
-        if(!(monthDuration==1 || monthDuration==3 || monthDuration==6)){
-            System.out.println("Enter a valid duration !!");
+        if(internetChoosenNumber>4){
+            System.out.println("Enter a valid sub-menu");
             internetService();
             return;
+        }
+        if(internetChoosenNumber==4) {
+            System.out.println("Returning to main menu");
+            return;
+        }
+
+        System.out.println("Enter Duration in months (1/3/6): ");
+        int monthDuration=scanner.nextInt();
+        while(!(monthDuration==1 || monthDuration==3 || monthDuration==6)){
+            System.out.println("Enter a valid duration !!");
+            System.out.println("Enter Duration in months (1/3/6): ");
+            monthDuration=scanner.nextInt();
         }
 
         double totalBill=0;
         switch (internetChoosenNumber){
             case 1:{
-                switch (monthDuration){
-                    case 1:{
-                        totalBill+=299;
-                        break;
-                    }
-                    case 3:{
-                        totalBill+=799;
-                        break;
-                    }
-                    case 6:{
-                        totalBill+=1499;
-                        break;
-                    }
-                }
+                studentInternetService(monthDuration,totalBill);
                 break;
             }
             case 2:{
-                switch (monthDuration){
-                    case 1:{
-                        totalBill+=499;
-                        break;
-                    }
-                    case 3:{
-                        totalBill+=1399;
-                        break;
-                    }
-                    case 6:{
-                        totalBill+=2699;
-                        break;
-                    }
-                }
+                homeInternetService(monthDuration,totalBill);
                 break;
             }
             case 3:{
-                switch (monthDuration){
-                    case 1:{
-                        totalBill+=999;
-                        break;
-                    }
-                    case 3:{
-                        totalBill+=2799;
-                        break;
-                    }
-                    case 6:{
-                        totalBill+=5499;
-                        break;
-                    }
-                }
+                businessInternetService(monthDuration,totalBill);
                 break;
             }
             default:{
                 System.out.println("Enter a Valid Input!! ");
                 internetService();
+            }
+        }
+
+    }
+
+    private static void businessInternetService(int monthDuration, double totalBill) {
+        switch (monthDuration){
+            case 1:{
+                totalBill=999;
+                break;
+            }
+            case 3:{
+                totalBill=2799;
+                break;
+            }
+            case 6:{
+                totalBill=5499;
+                break;
+            }
+        }
+        if(monthDuration==6){
+            double discount=totalBill*0.05;
+            totalBill-=discount;
+        }
+        System.out.println("Your Internet Bill is: "+totalBill);
+        System.out.println();
+    }
+
+    private static void homeInternetService(int monthDuration, double totalBill) {
+        switch (monthDuration){
+            case 1:{
+                totalBill=499;
+                break;
+            }
+            case 3:{
+                totalBill=1399;
+                break;
+            }
+            case 6:{
+                totalBill=2699;
+                break;
+            }
+        }
+        if(monthDuration==6){
+            double discount=totalBill*0.05;
+            totalBill-=discount;
+        }
+        System.out.println("Your Internet Bill is: "+totalBill);
+        System.out.println();
+    }
+
+    private static void studentInternetService(int monthDuration, double totalBill) {
+        switch (monthDuration){
+            case 1:{
+                totalBill=299;
+                break;
+            }
+            case 3:{
+                totalBill=799;
+                break;
+            }
+            case 6:{
+                totalBill=1499;
+                break;
             }
         }
         if(monthDuration==6){
@@ -141,65 +175,81 @@ public class SmartCityUtilitySystem {
         System.out.println("4. Back");
 
         int waterChoosenNumber=scanner.nextInt();
-        if(waterChoosenNumber==4)return;
-        boolean invalidInput=false;
 
-        double totalBill=0;
         switch (waterChoosenNumber){
             case 1:{
-                System.out.print("Enter number of persons: ");
-                int numberOfPerson=scanner.nextInt();
-                if(numberOfPerson<0) {
-                    invalidInput=true;
-                    System.out.println("Enter persons in positive");
-                    waterService();
-                    break;
-                }
-                totalBill+=(30*numberOfPerson);
+                residentialWaterService();
                 break;
             }
             case 2:{
-                System.out.print("Enter number of taps: ");
-                int numberOfTaps=scanner.nextInt();
-                if(numberOfTaps<0) {
-                    invalidInput=true;
-                    System.out.println("Enter taps in positive");
-                    waterService();
-                    break;
-                }
-                totalBill+=(25*numberOfTaps);
+                societyWaterService();
                 break;
             }
             case 3:{
-                System.out.print("Enter number of machines: ");
-                int numberOfMachines=scanner.nextInt();
-                if(numberOfMachines<0) {
-                    invalidInput=true;
-                    System.out.println("Enter machines in positive");
-                    waterService();
-                    break;
-                }
-                totalBill+=(100*numberOfMachines);
+                factoryWaterService();
                 break;
             }
             case 4:{
-                break;
+                System.out.println("Returning to main menu");
+                return;
             }
             default:{
-                invalidInput=true;
                 System.out.println("Enter a valid input !!");
                 waterService();
             }
         }
 
+    }
+
+    private static void factoryWaterService() {
+        double totalBill=0;
+        System.out.print("Enter number of machines: ");
+        int numberOfMachines=scanner.nextInt();
+        while(numberOfMachines<0) {
+            System.out.println("Enter machines in positive");
+            System.out.print("Enter number of machines: ");
+            numberOfMachines=scanner.nextInt();
+        }
+        totalBill+=(100*numberOfMachines);
         if(totalBill>3000){
             double tax=totalBill*0.08;
             totalBill+=tax;
         }
-        if(!invalidInput){
-            System.out.println("Your Total Water Bill is: "+totalBill);
-        }
+        System.out.println("Your Total Water Bill is: "+totalBill);
+    }
 
+    private static void societyWaterService() {
+        double totalBill=0;
+        System.out.print("Enter number of taps: ");
+        int numberOfTaps=scanner.nextInt();
+        while(numberOfTaps<0) {
+            System.out.println("Enter taps in positive");
+            System.out.print("Enter number of taps: ");
+            numberOfTaps=scanner.nextInt();
+        }
+        totalBill+=(25*numberOfTaps);
+        if(totalBill>3000){
+            double tax=totalBill*0.08;
+            totalBill+=tax;
+        }
+        System.out.println("Your Total Water Bill is: "+totalBill);
+    }
+
+    private static void residentialWaterService() {
+        double totalBill=0;
+        System.out.print("Enter number of persons: ");
+        int numberOfPerson=scanner.nextInt();
+        while(numberOfPerson<0) {
+            System.out.println("Enter persons in positive");
+            System.out.print("Enter number of persons: ");
+            numberOfPerson=scanner.nextInt();
+        }
+        totalBill+=(30*numberOfPerson);
+        if(totalBill>3000){
+            double tax=totalBill*0.08;
+            totalBill+=tax;
+        }
+        System.out.println("Your Total Water Bill is: "+totalBill);
     }
 
     private static void electricityService(){
@@ -212,108 +262,135 @@ public class SmartCityUtilitySystem {
         System.out.println("4. Back");
 
         int elecChoosenNumber=scanner.nextInt();
-        boolean invalidInput=true;
-        if(elecChoosenNumber==1 || elecChoosenNumber==2 || elecChoosenNumber==3 || elecChoosenNumber==4 ){
-            invalidInput=false;
-        }
-
-        double unitsConsumed=0;
-        double energyCharge=0;
-        double fixedCharge=0;
-        double extraAmount=0;
-        double totalBill=0;
-        boolean unitsChecker=false;
 
         switch (elecChoosenNumber){
             case 1:{
-                System.out.println("Enter Units Consumed: ");
-                unitsConsumed=scanner.nextInt();
-                if(unitsConsumed<0){
-                    unitsChecker=true;
-                }
-
-                double bill=0;
-                if(unitsConsumed>=0 && unitsConsumed<=100)energyCharge+=2*unitsConsumed;
-                else if(unitsConsumed>=101 && unitsConsumed<=300)energyCharge+=3*unitsConsumed;
-                else if(unitsConsumed>=301)energyCharge+=5*unitsConsumed;
-
-                fixedCharge=50;
-                bill=fixedCharge+energyCharge;
-
-                if(bill>2000) {
-                    extraAmount=0.1*bill;
-                    bill += extraAmount;
-                }
-
-                totalBill=bill;
-//                System.out.println("Connection type: Domestic Collection");
+                domesticCollection();
                 break;
             }
             case 2:{
-                System.out.println("Enter Units Consumed: ");
-                unitsConsumed=scanner.nextInt();
-                if(unitsConsumed<0){
-                    unitsChecker=true;
-                }
-
-                double bill=0;
-                if(unitsConsumed>=0 && unitsConsumed<=200)energyCharge+=5*unitsConsumed;
-                else if(unitsConsumed>=201 && unitsConsumed<=500)energyCharge+=7*unitsConsumed;
-                else if(unitsConsumed>=501)energyCharge+=10*unitsConsumed;
-
-                fixedCharge=150;
-                bill=fixedCharge+energyCharge;
-
-                totalBill=bill;
-//                System.out.println("Connection type: Commercial Collection");
+                commercialCollection();
                 break;
             }
             case 3:{
-                System.out.println("Enter Units Consumed: ");
-                unitsConsumed=scanner.nextInt();
-                if(unitsConsumed<0){
-                    unitsChecker=true;
-                }
-
-                double bill=0;
-                bill+=12*unitsConsumed;
-                fixedCharge=500;
-
-                if(unitsConsumed>1000){
-                    extraAmount=0.15*bill;
-                    bill-=extraAmount;
-                }
-                totalBill=bill;
-//                System.out.println("Connection type: Industrial Collection");
+                industrialCollection();
                 break;
+            }
+            case 4:{
+                System.out.println("Returning to main menu");
+                return;
             }
             default:{
                 System.out.println("Enter a valid input !!");
                 electricityService();
             }
         }
-        if(unitsChecker){
-            System.out.println("Please Enter Positive Units");
-            electricityService();
-            return;
-        }
-        if(!invalidInput){
-            if(elecChoosenNumber==1){
-                System.out.println("Connection Type: Domestic Connection");
-            }
-            else if(elecChoosenNumber==2){
-                System.out.println("Connection Type: Commercial Connection");
-            }
-            else if(elecChoosenNumber==3){
-                System.out.println("Connection Type: Industrial Connection");
-            }
-            System.out.println("Units Consumed: "+unitsConsumed);
-            System.out.println("Energy Charge: "+energyCharge);
-            System.out.println("Fixed Charge: "+fixedCharge);
-            System.out.println("Extra/Rebate: "+extraAmount);
-            System.out.println("Total Bill: "+totalBill);
 
+    }
+
+    private static void industrialCollection() {
+        double unitsConsumed=0;
+        double energyCharge=0;
+        double fixedCharge=0;
+        double extraAmount=0;
+        double totalBill=0;
+        System.out.println("Enter Units Consumed: ");
+        unitsConsumed=scanner.nextInt();
+        while(unitsConsumed<0){
+            System.out.println("Please Enter Positive Units");
+            System.out.println("Enter Units Consumed: ");
+            unitsConsumed=scanner.nextInt();
         }
+
+        double bill=0;
+        energyCharge+=12*unitsConsumed;
+        fixedCharge=500;
+        bill=energyCharge+fixedCharge;
+
+        if(unitsConsumed>1000){
+            extraAmount=0.15*bill;
+            bill-=extraAmount;
+        }
+        totalBill=bill;
+
+        System.out.println("Connection Type: Industrial Connection");
+        System.out.println("Units Consumed: "+unitsConsumed);
+        System.out.println("Energy Charge: "+energyCharge);
+        System.out.println("Fixed Charge: "+fixedCharge);
+        System.out.println("Extra/Rebate: "+extraAmount);
+        System.out.println("Total Bill: "+totalBill);
+    }
+
+    private static void commercialCollection() {
+        double unitsConsumed=0;
+        double energyCharge=0;
+        double fixedCharge=0;
+        double extraAmount=0;
+        double totalBill=0;
+        System.out.println("Enter Units Consumed: ");
+        unitsConsumed=scanner.nextInt();
+        while(unitsConsumed<0){
+            System.out.println("Please Enter Positive Units");
+            System.out.println("Enter Units Consumed: ");
+            unitsConsumed=scanner.nextInt();
+        }
+
+        double bill=0;
+        if(unitsConsumed<=200)energyCharge+=5*unitsConsumed;
+        else if(unitsConsumed>=201 && unitsConsumed<=500)energyCharge+=7*unitsConsumed;
+        else if(unitsConsumed>=501)energyCharge+=10*unitsConsumed;
+
+        fixedCharge=150;
+        bill=fixedCharge+energyCharge;
+
+        totalBill=bill;
+
+        System.out.println("Connection Type: Commercial Connection");
+        System.out.println("Units Consumed: "+unitsConsumed);
+        System.out.println("Energy Charge: "+energyCharge);
+        System.out.println("Fixed Charge: "+fixedCharge);
+        System.out.println("Extra/Rebate: "+extraAmount);
+        System.out.println("Total Bill: "+totalBill);
+
+    }
+
+    private static void domesticCollection() {
+        double unitsConsumed=0;
+        double energyCharge=0;
+        double fixedCharge=0;
+        double extraAmount=0;
+        double totalBill=0;
+        System.out.println("Enter Units Consumed: ");
+        unitsConsumed=scanner.nextInt();
+        while(unitsConsumed<0){
+            System.out.println("Please Enter Positive Units");
+            System.out.println("Enter Units Consumed: ");
+            unitsConsumed=scanner.nextInt();
+        }
+
+
+        double bill=0;
+        if(unitsConsumed<=100)energyCharge+=2*unitsConsumed;
+        else if(unitsConsumed>=101 && unitsConsumed<=300)energyCharge+=3*unitsConsumed;
+        else if(unitsConsumed>=301)energyCharge+=5*unitsConsumed;
+
+        fixedCharge=50;
+        bill=fixedCharge+energyCharge;
+
+        if(bill>2000) {
+            extraAmount=0.1*bill;
+            bill += extraAmount;
+        }
+
+        totalBill=bill;
+
+        System.out.println("Connection Type: Domestic Connection");
+        System.out.println("Units Consumed: "+unitsConsumed);
+        System.out.println("Energy Charge: "+energyCharge);
+        System.out.println("Fixed Charge: "+fixedCharge);
+        System.out.println("Extra/Rebate: "+extraAmount);
+        System.out.println("Total Bill: "+totalBill);
+
     }
 
 }
